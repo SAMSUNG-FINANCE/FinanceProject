0441fdaaf1eeec257f9ca01f2052c89d17963903
djc06048 <gsafe1213@gmail.com>
2023-05-17 10:41:03 +0900

    fix: 10개까지 뉴스 가져올 수 있게 스크롤 조정해 수정완료

--- news.py
+++ news.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException,NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 # WebDriver 객체 생성
 driver = webdriver.Chrome()
 
@@ -8,35 +10,85 @@ driver = webdriver.Chrome()
 url = "https://m.stock.naver.com/domestic/stock/005930/news/title"
 driver.get(url)
 driver.implicitly_wait(10)
+
 # 뉴스 기사들을 순회하면서 클릭
-news_links =driver.find_elements(By.CSS_SELECTOR,"#content > div:nth-child(4) > div:nth-child(3) > div:nth-child(2) > div.NewsList_article__2hkCL > ul")
+news_links =driver.find_elements(By.CSS_SELECTOR,"#content > div:nth-child(4) > div:nth-child(3) > div:nth-child(2) > div.NewsList_article__2hkCL > ul > li")
+
+# 뉴스 개수
+print(len(news_links))
+news_count=0
+
+for idx,news_link in enumerate(news_links):
+    # print("현재 뉴스 개수:", news_count)
+
+    # 10개까지만 가져온다
+    if(idx==10):
+        break
+
+    try:
+        actions = ActionChains(driver)
+        actions.move_to_element(news_link) #요소위로 마우스 이동
+        actions.click()
+        actions.perform()
+        
+        # news_link.click()
+
+        # 뉴스 제목
+        root_div = driver.find_element(By.TAG_NAME, "div")
+        news_title = root_div.find_element(By.CLASS_NAME, "NewsHeader_title__1vvDz").text
+        print("제목:", news_title)
+
+        # 뉴스 본문
+        root_div = driver.find_element(By.TAG_NAME, "div")
+        content_box_div = root_div.find_element(By.CLASS_NAME, "NewsEnd_contentBox__R7Ics")
+        news_content = content_box_div.find_element(By.CLASS_NAME, "NewsEnd_textEnd__xIOkX").text
+        print(news_content)
+
+        # 이후에 다시 기존 탭으로 전환
+        driver.back()
+        print("이전 url:", driver.current_url)
+
+        # 다음 뉴스 기사 클릭을 위해 대기
+        time.sleep(1)
+
+        news_count += 1
+    except StaleElementReferenceException:
+        print("StaleElementReferenceException 발생")
+        news_links = driver.find_elements(By.CSS_SELECTOR, "#content > div:nth-child(4) > div:nth-child(3) > div:nth-child(2) > div.NewsList_article__2hkCL > ul > li")
+        news_link = news_links[news_count]
+
+        actions = ActionChains(driver)
+        actions.move_to_element(news_link)
+        actions.click()
+        actions.perform()
+
+        # news_link.click()
+        try: 
+            # 뉴스 제목
+            root_div = driver.find_element(By.TAG_NAME, "div")
+            news_title = root_div.find_element(By.CLASS_NAME, "NewsHeader_title__1vvDz").text
+            print("제목:", news_title)
+
+            # 뉴스 본문
+            root_div = driver.find_element(By.TAG_NAME, "div")
+            content_box_div = root_div.find_element(By.CLASS_NAME, "NewsEnd_contentBox__R7Ics")
+            news_content = content_box_div.find_element(By.CLASS_NAME, "NewsEnd_textEnd__xIOkX").text
+            print(news_content)
+
+            # 이후에 다시 기존 탭으로 전환
+            driver.back()
+            print("이전 url:", driver.current_url)
+
+            # 다음 뉴스 기사 클릭을 위해 대기
+            time.sleep(1)
+        except NoSuchElementException:
+            # NoSuchElementException 발생 시 스크롤을 내리고 다시 요소를 찾음
+            print("NoSuchElementException 발생")
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
 
-for news_link in news_links:
-    
-    # 뉴스 기사 링크 클릭
-    news_link.click()
-    
-    # 새로운 탭으로 전환
-    driver.switch_to.window(driver.window_handles[-1])
-    
-    # 기사 제목과 내용 출력
-    news_title = driver.find_element(By.TAG_NAME,"strong").text
-    print("제목:", news_title)
 
-    root_div = driver.find_element(By.TAG_NAME, "div")
-    content_box_div = root_div.find_element(By.CLASS_NAME, "NewsEnd_contentBox__R7Ics")
-    print(content_box_div.text)
-    # news_content = driver.find_element(By.CSS_SELECTOR,"div.articleCont").text
 
-    # print("내용:", news_content)
-    
-    # 기사 내용 출력 후 다시 원래 탭으로 전환
-    driver.close()
-    driver.switch_to.window(driver.window_handles[-1])
-    time.sleep(1)
-    
-    # 다음 뉴스 기사로 이동
-    driver.execute_script("arguments[0].scrollIntoView();", news_link)
-    driver.execute_script("window.scrollBy(0, -150);")
-    time.sleep(1)
+    news_count += 1
+
+driver.quit()
